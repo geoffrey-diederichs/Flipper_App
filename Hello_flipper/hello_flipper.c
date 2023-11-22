5c8d5293aca3f97d98196df52a03d7ae92cb1979
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-11-22 09:53:56 +0100

    hello_flipper.c done ?

--- Hello_flipper/hello_flipper.c
+++ Hello_flipper/hello_flipper.c
@@ -11,10 +11,10 @@ static void draw(Canvas* canvas, void* ctx)
 	canvas_clear(canvas);
 
 	canvas_set_font(canvas, FontPrimary);
-   	canvas_draw_str(canvas, 32, 13, "My First App");
+   	canvas_draw_str(canvas, 32, 13, "Hello Flipper");
 
 	canvas_set_font(canvas, FontSecondary);
-	canvas_draw_str(canvas, 15, 40, " press back to exit FAP");
+	canvas_draw_str(canvas, 15, 40, " press back to exit ");
 	
 	canvas_draw_line(canvas, 2, 23, 126, 23);
 }
@@ -27,8 +27,43 @@ static void input(InputEvent* input_event, void* ctx)
 	furi_message_queue_put(event_queue, input_event, FuriWaitForever);
 }
 
+// Main function
 int hello_flipper_main(void* p)
 {
 	UNUSED(p);
+	
+	// Variables storing events sent by the Flipper API
+	InputEvent event;
+	FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+	
+	// Setting the function meant to draw the GUI
+	ViewPort* view_port = view_port_alloc();
+	view_port_draw_callback_set(view_port, draw, NULL);
+	
+	// Setting the function meant to handle user inputs
+	view_port_input_callback_set(view_port, input, event_queue);
+	
+	// Don't ask me what this does it's required to set the GUI
+	Gui* gui = furi_record_open(RECORD_GUI);
+	gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
+	while(true)
+	{
+		// Checking for new events
+		furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
+		
+		// If the event is that the user pressed the back key
+		if (event.key == InputKeyBack)
+		{
+			break;
+		}
+	}
+
+	// Freeing everything before exiting
+	furi_message_queue_free(event_queue);
+	gui_remove_view_port(gui, view_port);
+	view_port_free(view_port);
+	furi_record_close(RECORD_GUI);
+
+	return 0;
 }
